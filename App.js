792d1a2f244996154fcf264811a2ab74a84a1548
Albin <albinjiji4@gmail.com>
2021-08-03 13:42:02 +0530

    first committ

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hi albin</Text>
+      <Text>hi albi</Text>
       <StatusBar style="auto" />
     </View>
   );
