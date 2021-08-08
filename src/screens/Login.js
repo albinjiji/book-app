4f2336dfcd93eb687cddc138acc5e42902a06a2e
Albin <albinjiji4@gmail.com>
2021-08-08 16:11:25 +0530

    4 commit

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -4,6 +4,7 @@ import { Text, SafeAreaView, StyleSheet, Image, TextInput, View, Dimensions} fro
 const { width: WIDTH } = Dimensions.get('window') 
 export default class Login extends React.Component{
     render(){
+        const {navigate} = this.props.navigation
         return(
             <SafeAreaView style={styles.container}>
                 <Image source = {require('../images/image.jpg')} 
@@ -11,13 +12,16 @@ export default class Login extends React.Component{
             <Text style={styles.fontText}>Book App</Text>
             <View style={styles.form}>
             <TextInput style={styles.emailText} placeholder={'Email'} 
-                        placeholderTextColor={'rgba(0,0,7)'}
                         underlineColorAndroid='transparent' /></View>
 
             <View style={styles.form}>
             <TextInput style={styles.passText} placeholder={'Password'} 
-                        placeholderTextColor={'rgba(0,0,255)'}
-                        underlineColorAndroid='transparent' /></View>
+                        underlineColorAndroid='transparent'  secureTextEntry /></View>
+            <View style={styles.login}>
+                <Text style={styles.test}>       Login</Text>
+            </View>
+            <View style={styles.login}><Text style={styles.test} onPress={()=>navigate ('Register')}>     Register</Text>
+            </View>
 
             </SafeAreaView>
         )
@@ -44,11 +48,15 @@ const styles = StyleSheet.create({
         width: WIDTH - 55,
         height: 45,
         borderRadius: 45,
-        fontSize: 16,
+        fontSize: 18,
         paddingLeft: 45,
         paddingHorizontal: 10,
         color: "black",
+        marginTop: 25,
+        backgroundColor: "#00716f",
         marginHorizontal: 25,
+        borderRadius: 25,
+        borderColor: '#4287f5',
     },
     form:{
         paddingHorizontal: 10,
@@ -59,10 +67,32 @@ const styles = StyleSheet.create({
         width: WIDTH - 55,
         height: 45,
         borderRadius: 45,
-        fontSize: 16,
+        backgroundColor: "#00716f",
+        fontSize: 18,
         paddingLeft: 45,
         paddingHorizontal: 10,
         color: "black",
         marginHorizontal: 25,
+        marginTop: 15,
+    },
+    login:{
+        marginHorizontal: 25,
+        borderRadius: 25,
+        borderColor: '#4287f5',
+        marginTop: 25,
+        backgroundColor: "#8d42f5",
+        height: 50,
+        width: 200,
+        paddingLeft: 45,
+        paddingHorizontal: 10,
+        justifyContent: "center",
+        fontSize: 19,
+        fontWeight: "bold",
+        marginTop: 15,
+    },
+    test: {
+        justifyContent: "center",
+        fontSize: 19,
+        fontWeight: "bold",
     }
 })
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,10 +1,98 @@
 import React from 'react';
-import { Text, View} from 'react-native';
+import { Text, SafeAreaView, StyleSheet, Image, TextInput, View, Dimensions} from 'react-native';
 
-export default class Redister extends React.Component{
+const { width: WIDTH } = Dimensions.get('window') 
+export default class Register extends React.Component{
     render(){
+        const {navigate} = this.props.navigation
         return(
-            <Text>Register</Text>
+            <SafeAreaView style={styles.container}>
+                <Image source = {require('../images/image.jpg')} 
+                style = {{width:"100%", height:"40%"}}  />
+            <Text style={styles.fontText}>Book App</Text>
+            <View style={styles.form}>
+            <TextInput style={styles.emailText} placeholder={'Email'} 
+                        underlineColorAndroid='transparent' /></View>
+
+            <View style={styles.form}>
+            <TextInput style={styles.passText} placeholder={'Password'} 
+                        underlineColorAndroid='transparent'  secureTextEntry />
+                        <TextInput style={styles.passText} placeholder={'Confirm Password'} 
+                        underlineColorAndroid='transparent'  secureTextEntry /></View>
+            <View style={styles.login}>
+                <Text style={styles.test} onPress={()=>navigate ('Login')} >     Register</Text>
+            </View>
+
+            </SafeAreaView>
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: '#fff',
+        height:"100%",
+        justifyContent: "center",
+        
+    },
+    fontText: {
+        fontSize: 28,
+        fontWeight: "bold",
+        marginTop: 10,
+        opacity: 0.5,
+    },
+    emailText: {
+        width: WIDTH - 55,
+        height: 45,
+        borderRadius: 45,
+        fontSize: 18,
+        paddingLeft: 45,
+        paddingHorizontal: 10,
+        color: "black",
+        marginTop: 25,
+        backgroundColor: "#00716f",
+        marginHorizontal: 25,
+        borderRadius: 25,
+        borderColor: '#4287f5',
+    },
+    form:{
+        paddingHorizontal: 10,
+        paddingVertical: 2,
+        borderRadius: 23,
+    },
+    passText: {
+        width: WIDTH - 55,
+        height: 45,
+        borderRadius: 45,
+        backgroundColor: "#00716f",
+        fontSize: 18,
+        paddingLeft: 45,
+        paddingHorizontal: 10,
+        color: "black",
+        marginHorizontal: 25,
+        marginTop: 15,
+    },
+    login:{
+        marginHorizontal: 25,
+        borderRadius: 25,
+        borderColor: '#4287f5',
+        marginTop: 25,
+        backgroundColor: "#8d42f5",
+        height: 50,
+        width: 200,
+        paddingLeft: 45,
+        paddingHorizontal: 10,
+        justifyContent: "center",
+        fontSize: 19,
+        fontWeight: "bold",
+        marginTop: 15,
+    },
+    test: {
+        justifyContent: "center",
+        fontSize: 19,
+        fontWeight: "bold",
+    }
+})
